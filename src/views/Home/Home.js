1954ddfc358e3a4e8f4ee43258a7207864ee31b0
pammypoor <36018468+pammypoor@users.noreply.github.com>
2022-05-18 20:25:38 -0700

    Initial Routes

---
+++ src/views/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Home extends React.PureComponent {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return (
+            <div className = "home-wrapper">
+                TEST
+            </div>
+        )
+    }
+}
+
+export default Home;
